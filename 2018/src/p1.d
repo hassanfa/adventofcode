11b9c081266a0510ec87e5fd9a6519aba902cdb3
hassanfa <hassan.foroughi@gmail.com>
2018-12-09 21:07:44 +0100

    first part done

---
+++ 2018/src/p1.d
@@ -0,0 +1,40 @@
+import std.stdio;  
+import std.string;
+import std.conv;
+
+void main(string[] args) { 
+  int s = 0;
+  int f = 0; // part two
+  int p = 0; // part one
+  int[string] s_t;
+
+  s_t[to!string(s)] = 1;
+
+  while ( f == 0 ) {
+
+    auto file = File("../data/p1", "r");
+    while ( !file.eof() ) {
+      string line = chomp(file.readln());
+
+      if ( !file.eof() ) {
+        s += to!int(line);
+        s_t[to!string(s)] += 1;
+        if ( s_t[to!string(s)] == 2 && f == 0) {
+          writeln("first twice is ", s);
+          f = 1;
+        }
+      }
+    }
+ 
+    p += 1;
+    if ( p == 1){
+      writeln("part 1 answer is:", s);
+    }
+//    break;
+  }  
+
+  foreach (e; s_t.byKeyValue) {
+    //writeln(e.key, ": ", e.value);
+  }
+  //writeln(s);
+}
